17a61c1e71db885e42b23f7c8c123a01f92613bd
surajgsn07 <surajgn07@gmail.com>
2024-09-05 17:40:06 +0530

    config added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
-
+require('dotenv').config();
 const app = express();
 
 // Configure CORS to allow requests from localhost:5173
